9d5c67c819277659719f688a95a6f65ee7171dee
ciulicDev <angelo.prospero.developer@gmail.com>
2026-02-14 12:47:25 +0100

    invio dei post e creazione dei report

--- main.py
+++ main.py
@@ -20,3 +20,34 @@ with open(input_file, newline="") as f:
                 "email":["email"]
             })
         })
+
+#lista per salvare risposte
+risposte =[]
+
+#invio POST per ciascun utente
+
+for u in utenti:
+    response = requests.post(api_url,json=u)
+    if response.status_code == 201:
+        risposte.append(response.json())
+        print(f"{u["name"]} inviato con successo")
+    else:
+        risposte.append({
+            "name": u["name"],
+            "error": response.status_code
+        })
+        print(f"Errore per {u["nome"]}:{response.status_code}")
+
+#scrittura report JSON
+with open("report_api.json", "w") as f:
+    json.dump(risposte, f, indent=2)
+
+#scrittura report CSV
+with open("report_api.csv", "w", newline="") as f:
+    campi = risposte[0].keys() #usa chiavi del primo elemento
+    writer = csv.DictWriter(f, fieldnames=campi)
+    writer.writeheader
+    for r in risposte:
+        writer.writerow(r)
+
+print("report csv e json creati")
